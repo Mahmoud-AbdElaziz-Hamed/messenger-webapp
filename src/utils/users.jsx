d78b8806af3c15855737e2465cea47980b4dee2d
unknown <mahmoudabdelaziz93.mz@gmail.com>
2024-01-24 20:58:05 +0200

    feat: add mock data

---
+++ src/utils/users.jsx
@@ -0,0 +1,20 @@
+export const usersMock = [
+  {
+    id: 1,
+    username: "mahmoud abd elaziz hamed",
+    email: "mahmoud@gmail.com",
+    password: "12345",
+  },
+  {
+    id: 2,
+    username: "yasser elhennawi",
+    email: "yasser@gmail.com",
+    password: "123456",
+  },
+  {
+    id: 3,
+    username: "ahmed abd elaziz hamed",
+    email: "ahmed@gmail.com",
+    password: "123457",
+  },
+];
